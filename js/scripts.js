ad3926eeaca5ed795174a1ba17f11ca583e6db76
Karthik Sangam <ksangam@nabancard.com>
2023-06-14 15:01:31 -0400

    fix: some more configurations

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// Initialize the DOM.
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Create the header.
+    var header = document.createElement("header");
+    header.innerHTML = "This is the header";
+  
+    // Create the content.
+    var content = document.createElement("section");
+    content.innerHTML = "Coming Soon";
+  
+    // Create the footer.
+    var footer = document.createElement("footer");
+    footer.innerHTML = "This is the footer";
+  
+    // Append the header, content, and footer to the document body.
+    document.body.appendChild(header);
+    document.body.appendChild(content);
+    document.body.appendChild(footer);
+  });
